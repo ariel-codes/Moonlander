6a2954b38d4ce74c77683fa3d77843e88d105761
Ariel Santos <as-ariel@outlook.com>
2016-02-21 14:39:41 -0300

    Initial Commit

---
+++ src/moonbase/beta/MoonBaseBeta.java
@@ -0,0 +1,16 @@
+package moonbase.beta;
+
+/**
+ *
+ * @author ariel
+ */
+public class MoonBaseBeta {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
