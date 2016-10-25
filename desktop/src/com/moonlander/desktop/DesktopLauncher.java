bfae458462e2fe2d37e58ad91dd899cdeede12ba
asantos07 <as-ariel@outlook.com>
2016-10-25 15:07:06 -0200

    Resetei o projeto

--- desktop/src/com/moonlander/desktop/DesktopLauncher.java
+++ desktop/src/com/moonlander/desktop/DesktopLauncher.java
@@ -1,12 +1,12 @@
-package com.asantos.moonbase.desktop;
+package com.moonlander.desktop;
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
-import com.asantos.moonbase.MoonbaseBeta;
+import com.moonlander.GameLauncher;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		new LwjglApplication(new MoonbaseBeta(), config);
+		new LwjglApplication(new GameLauncher(), config);
 	}
 }
